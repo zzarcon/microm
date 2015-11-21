6f9bd4d10a7e04cc35fcafd9de6b9e740838a836
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-21 18:42:13 +0100

    Setting testsuite

---
+++ test/specs.js
@@ -0,0 +1,8 @@
+var assert = chai.assert;
+var expect = chai.expect;
+
+describe('Microm', function() {
+  it('Should exist', function() {
+    assert.equal(typeof window.Microm !== "undefined", true);
+  });
+});
